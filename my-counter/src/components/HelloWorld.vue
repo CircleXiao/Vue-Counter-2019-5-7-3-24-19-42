4b9b37e241b2f71b7ea90c713108d4dbca68339f
mmy <mmy_xize@163.com>
2019-09-02 10:53:37 +0800

    commit counter project

---
+++ my-counter/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <button v-on:click="addHandle">+</button>
+    <button v-on:click="minusHandle">-</button>
+    <span>{{ num }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      num: 0
+    }
+  },
+  methods: {
+    addHandle: function () {
+      this.num++;
+    },
+    minusHandle: function () {
+      this.num--;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
